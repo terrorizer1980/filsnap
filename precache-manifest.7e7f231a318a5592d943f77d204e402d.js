bcb3928ece3ddbdcdb4d614a4539ad688ed1c642
Erik Marks <rekmarks@protonmail.com>
2021-04-04 15:33:14 -0700

    Updates

---
+++ precache-manifest.7e7f231a318a5592d943f77d204e402d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e139ef0757027d588636cff88ef0d777",
+    "url": "/filsnap/index.html"
+  },
+  {
+    "revision": "0b2c80533a9c1ffbf444",
+    "url": "/filsnap/static/css/main.1231a5ef.chunk.css"
+  },
+  {
+    "revision": "e258b9a4790075b7ef19",
+    "url": "/filsnap/static/js/2.8673c812.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/filsnap/static/js/2.8673c812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2c80533a9c1ffbf444",
+    "url": "/filsnap/static/js/main.7a0bf631.chunk.js"
+  },
+  {
+    "revision": "53b59f0ca11a825aae3b",
+    "url": "/filsnap/static/js/runtime-main.325620b8.js"
+  },
+  {
+    "revision": "3ff37f052bd81473a2f75e641a3d4aec",
+    "url": "/filsnap/static/media/filecoin_logo.3ff37f05.png"
+  },
+  {
+    "revision": "d9e0e004070562c54c3b0fd13df48e75",
+    "url": "/filsnap/static/media/node_factory_logo.d9e0e004.png"
+  }
+]);
